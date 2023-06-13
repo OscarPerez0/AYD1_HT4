c749cfd00f6a91820204dfb797f3662d92895b96
fer-jesus <limajesus909@gmail.com>
2023-06-13 04:49:43 -0600

    Update suma.js

--- suma.js
+++ suma.js
@@ -21,3 +21,24 @@ app.post('/sum', (req, res) => {
 app.listen(4000, () => {
   console.log('Server is running on port 3000');
 });
+
+
+
+
+
+// Endpoint for summing two numbers
+app.post('/rest', (req, res) => {
+  const { num1, num2 } = req.body;
+
+  if (typeof num1 !== 'number' || typeof num2 !== 'number') {
+    return res.status(400).json({ error: 'Both num1 and num2 should be numbers' });
+  }
+
+  const rest = num1 - num2;
+  return res.json({ result: rest });
+});
+
+// Start the server
+app.listen(4000, () => {
+  console.log('Server is running on port 3000');
+});
